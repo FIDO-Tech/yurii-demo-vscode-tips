ca51e6391ac191059e822c75f73a7ddaaff36700
Yurii Zaporozhchenko <yurnero017@gmail.com>
2025-07-11 21:54:56 +0100

    add minor comments and alias section

--- 4-git/todo.js
+++ 4-git/todo.js
@@ -1,7 +1,18 @@
 /*
- * Git blame
+ * Git blame: 
+ * - install GitLens and press Ctrl + Shift + G, B (B is pressed after Ctrl + Shift + G)
  */
 
 /*
- * Git compare
+ * Git compare: files can be compared in VSCode, much better UI 
+ * - open command palette (Ctrl + Shift + P) and then type "Compare against...", explore
+ * - use GitLens extension (https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens) with Rich Hovers to compare commits
+ */
+
+
+/* 
+ * Notes: 
+ *
+ * Using terminal is faster than using Desktop app for managing git.
+ * Learning and getting used to terminal commands is recommended.
  */
---
+++ 6-speedy-workflow/index.js
@@ -0,0 +1,10 @@
+/*
+ * Aliases (shortcuts for commands in your OS)
+ *
+ * - gs: git status
+ * - gch: git checkout
+ * - gc: git commit
+ * - gp: git push
+ * - g: git
+ *
+ */
